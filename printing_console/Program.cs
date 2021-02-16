8f0023f080d8ff2c55fd0cea550a10ee4d44a464
leoharrisn <dbahenalopez@hotmail.com>
2021-02-16 16:30:50 -0600

    Created project printing console

---
+++ printing_console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace printing_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region test
+                Console.WriteLine("Hello World!");
+
+
+                
+            #endregion
+            
+        }
+    }
+}
