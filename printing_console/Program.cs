019449c81d656ded0f11966e34ccf5b3ef30aca0
leoharrisn <dbahenalopez@hotmail.com>
2021-02-18 10:12:02 -0600

    Array of any kind created

--- printing_console/Program.cs
+++ printing_console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using static System.Console;
 namespace printing_console
 {
     class Program
@@ -7,10 +7,20 @@ namespace printing_console
         static void Main(string[] args)
         {
 
-            #region test
-                Console.WriteLine("Hello World!");
+            #region arrayOfInformation
+                
+                var dataTypes = new dynamic [11];       //An array of dynamic is created allowing to contain any kind of types
 
+                sbyte firstType = 0;
+                byte  secondType = 0;
+                dataTypes [0] = firstType;
+                dataTypes [1] = secondType;
+                
+               
+                    WriteLine($"Soy un sbyte con valor de {dataTypes[0]}\n");
+                    WriteLine($"Soy un byte con valor de {dataTypes[1]}\n");
 
+              
                 
             #endregion
             
