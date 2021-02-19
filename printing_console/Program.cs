2551e20cb8f8d1ec0e63c6296199b3f130a14b4d
leoharrisn <dbahenalopez@hotmail.com>
2021-02-18 19:24:29 -0600

    The code is commented

--- printing_console/Program.cs
+++ printing_console/Program.cs
@@ -10,12 +10,13 @@ namespace printing_console
 
             #region arrayOfInformation
             
-                string [] nameOfTypes;
-                nameOfTypes = new string [11];
+                string [] nameOfTypes;              //Declaration of a string array
+               
+                nameOfTypes = new string [11];      //The size of the string is assigned
 
-                nameOfTypes [0] = "sbyte";
-                nameOfTypes [1] = "byte";
-                nameOfTypes [2] = "short";
+                nameOfTypes [0] = "sbyte";          //The names of the types will be assigned to
+                nameOfTypes [1] = "byte";           //each position of the collection
+                nameOfTypes [2] = "short";          
                 nameOfTypes [3] = "ushort";
                 nameOfTypes [4] = "int";
                 nameOfTypes [5] = "uint";
@@ -28,13 +29,17 @@ namespace printing_console
             #endregion
 
             #region printingContents
+            
 
-            Line();
+            //In order to print a divisor line, the Line mehod is invocated
+            Line();         
 
-            WriteLine($"{"Type",-10}{"Byte(s) of memory",-10}{"Min",24}{"Max",31}");
+            WriteLine($"{"Type",-10}{"Byte(s) of memory",-10}{"Min",24}{"Max",31}"); //this prints the titles of the table
              
             Line ();
            
+            //the cycle will print the table with the information of the array nameOfTypes and the method InfoOfType
+
             for (int i = 0; i < 11; i++){
 
                 WriteLine($"{nameOfTypes[i],-10}{InfoOfType(i)}");
@@ -50,14 +55,22 @@ namespace printing_console
 
         }
     #region methodOfTable
+
+
+        // The method InfoOfType returns a string with the number of bytes,
+        // the min value & the max value of the corresponding type
         private static string InfoOfType (int counter){
 
-                string information = string.Empty;
-                int decision = counter;
+                string information = string.Empty;  //declaration of the string information as empty
+               
+                int decision = counter;             //the int decision will receive the value of the
+                                                    //current position in the nameOfTypes Array 
 
-                switch (decision)
+                //decision will switch with the position of the 
+                //array
+                switch (decision)                 
                 {
-
+                    //In every case, information will return a string with the memory, min & max value
                     case 0:
                      information = $"{sizeof(sbyte), -10}{sbyte.MinValue, 31}{sbyte.MaxValue, 31}";
                      break;
@@ -112,7 +125,8 @@ namespace printing_console
         #endregion
    
     #region methodOfDivison
-        
+
+        //this method prints a line made with the "-" character       
         private static void Line (){
 
              for (int i = 0; i < 82; i++){
